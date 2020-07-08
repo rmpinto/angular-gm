d5e1d9d08c21227b9ce3525c97cacbf7855530cd
rmpinto <52088176+rmpinto@users.noreply.github.com>
2020-07-08 01:19:41 +0100

    Install dependencies for MatSlider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule, MatIconModule, MatListModule, MatButtonModule } from '@angular/material';
+import { MatInputModule } from '@angular/material/input';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserModule, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+import { GestureConfig } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { GmMainComponent } from './gm-main/gm-main.component';
 import { GmMainNavComponent } from './gm-main-nav/gm-main-nav.component';
 import { GmMainRollerComponent } from './gm-main-roller/gm-main-roller.component';
@@ -15,18 +19,19 @@ import { GmMainRollerComponent } from './gm-main-roller/gm-main-roller.component
 @NgModule({
 	declarations: [ AppComponent, GmMainComponent, GmMainNavComponent, GmMainRollerComponent ],
 	imports: [
-		BrowserModule,
+		BrowserAnimationsModule,
+		MatSliderModule,
 		AppRoutingModule,
-		NoopAnimationsModule,
 		FlexLayoutModule,
 		MatToolbarModule,
-		MatSidenavModule,
 		MatListModule,
 		MatButtonModule,
 		MatIconModule,
-		MatExpansionModule
+		MatExpansionModule,
+		MatInputModule,
+		MatSelectModule
 	],
-	providers: [],
+	providers: [ { provide: HAMMER_GESTURE_CONFIG, useClass: GestureConfig } ],
 	bootstrap: [ AppComponent ]
 })
 export class AppModule {}
