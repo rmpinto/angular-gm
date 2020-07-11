e3d73480ecebd63c8611f5d55e17fe7b03e28a4a
rmpinto <52088176+rmpinto@users.noreply.github.com>
2020-07-11 16:27:53 +0100

    Create map component

---
+++ src/app/gm-main-map/gm-main-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gm-main-map',
+  templateUrl: './gm-main-map.component.html',
+  styleUrls: ['./gm-main-map.component.scss']
+})
+export class GmMainMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
